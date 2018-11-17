329260b7d0981f05e32cb9c65f39af455e855849
Ganesh Pol <polganesh@gmail.com>
2018-11-17 18:00:23 +0000

    changes

--- virtual-network/main.tf
+++ virtual-network/main.tf
@@ -58,19 +58,5 @@ resource "azurerm_subnet" "public" {
 	count			=	"${length(var.public_subnet_cidr_list)}"
 	name			=	"sub-${var.location_id}-${var.environment}-${var.cost_centre}-VNet${var.vnet_seq_id}-pub-${count.index}-${var.seq_id}"
 	address_prefix		=	"${element(var.public_subnet_cidr_list, count.index)}"
-	tags {
-		Name		=	"sub-${var.location_id}-${var.environment}-${var.cost_centre}-VNet${var.vnet_seq_id}-pub-${count.index}-${var.seq_id}"
-    		Environment 	= 	"${var.environment}"
-		LocationId	=	"${var.location_id}"
-		Location	=	"${var.location}"
-		CostCentre	=	"${var.cost_centre}"
-		Project		=	"${var.project}"
-		VersionId	=	"${var.version_id}"
-		BuildDate	=	"${var.build_date}"
-		MaintenanceDay	=	"${var.maintenance_day}"
-		MaintenanceTime	=	"${var.maintenance_time}"
-		SeqId		=	"${var.seq_id}"
-		VnetSeqId	=	"${var.vnet_seq_id}"
-	}
 }
 
